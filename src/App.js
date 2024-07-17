85f1eb87816ac7ce456477b59c028a3c8a3209d8
Bink <tuanbinhnguyenngoc@gmail.com>
2024-07-17 16:34:00 +0700

    exp pagination for react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  Card,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TablePagination,
+} from "@mui/material";
+
+const Passengers = () => {
+  const [passengerList, setPassengerList] = useState([]);
+  const [passengerCount, setPassengerCount] = useState(0);
+  const [controller, setController] = useState({ page: 0, rowsPerPage: 10 });
+
+  useEffect(() => {
+    const getData = async () => {
+      const url = `https://api.instantwebtools.net/v1/passenger?page=${controller.page}&size=${controller.rowsPerPage}`;
+      try {
+        const res = await fetch(url);
+        if (res.statusText === "OK") {
+          const data = await res.json();
+          setPassengerList(data.data);
+          setPassengerCount(data.totalPassengers);
+        }
+      } catch (err) {
+        console.log("Error while fetching: ", err);
+      }
+    };
+    getData();
+  }, [controller]);
+
+  const handlePageChange = (event, newpage) => {
+    setController({ ...controller, page: newpage });
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setController({
+      ...controller,
+      rowsPerPage: parseInt(event.target.value, 10),
+      page: 0,
+    });
+  };
+
+  return (
+    <Card>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Trips</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {passengerList.map((passenger) => (
+            <TableRow>
+              <TableCell>{passenger.name}</TableCell>
+              <TableCell>{passenger.trips}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        component="div"
+        onPageChange={handlePageChange}
+        page={controller.page}
+        count={passengerCount}
+        rowsPerPage={controller.rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Card>
+  );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Passengers />;
 }
 
 export default App;
